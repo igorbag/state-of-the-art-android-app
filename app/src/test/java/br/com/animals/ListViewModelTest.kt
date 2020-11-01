88b1f09e1f10bf3f4d4422cf361052413f286454
igor.bagliotti <igor.bagliotti@invillia.com>
2020-11-01 16:57:26 -0300

    Create the init test config.

---
+++ app/src/test/java/br/com/animals/ListViewModelTest.kt
@@ -0,0 +1,30 @@
+package br.com.animals
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Before
+import org.junit.Rule
+import java.util.concurrent.Executor
+
+class ListViewModelTest {
+    @get:Rule
+
+    var rule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUpRxSchedulers() {
+        val immediate = object : Scheduler() {
+            override fun createWorker(): Worker {
+                return ExecutorScheduler.ExecutorWorker(Executor {
+                    it.run()
+                }, true)
+            }
+        }
+
+        RxJavaPlugins.setInitNewThreadSchedulerHandler { scheudler -> immediate }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler{ scheduler -> immediate}
+    }
+}
